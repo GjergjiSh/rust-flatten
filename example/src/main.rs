37f01e458fc2e1edf082859ff8e00767ce74942f
GjergjiSh <gjergji_shkurti@yahoo.com>
2024-06-22 12:49:03 +0200

    implement fmt debug for parent in a way that prints out in A2L format

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+
+struct Parent {
+    uid: u32,
+    child: Child,
+}
+
+#[derive(Debug)]
+struct Child {
+    uid: u32,
+}
+
+impl std::fmt::Debug for Parent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Parent")
+            .field("uid", &self.uid)
+            .field("Child.uid", &self.child.uid)
+            .finish()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let parent = Parent {
+        uid: 1,
+        child: Child { uid: 2 },
+    };
+
+    dbg!(parent);
 }
