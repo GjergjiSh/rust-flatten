9a8bf636cd98d92db13f83f43146eaa72dd90710
GjergjiSh <gjergji_shkurti@yahoo.com>
2024-06-22 14:52:15 +0200

    output is correct

--- flatten/flatten_derive/src/lib.rs
+++ flatten/flatten_derive/src/lib.rs
@@ -10,27 +10,28 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields};
 #[proc_macro_derive(Flatten)]
 pub fn flatten_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let struct_name = input.ident;
+    let struct_name = &input.ident;
 
     let gen = match input.data {
         Data::Struct(data_struct) => {
             let field_handlers = data_struct.fields.iter().map(|field| {
-                let field_name = field.ident.as_ref().unwrap();
+                let field_name = &field.ident;
                 let field_type = &field.ty;
-                let qualified_name = format!("{}.{}", struct_name, field_name);
-
+            
                 quote! {
-                    characteristics.push(Characteristic {
-                        name: #qualified_name.to_string(),
-                        datatype: stringify!(#field_type).to_string(),
-                    });
-
-                    // Attempt to call to_a2l on the field if it implements ToA2l, appending its characteristics
+                    // Check if the field type implements Flatten and if so, call to_a2l_optional
                     if let Some(nested_characteristics) = <#field_type as Flatten>::to_a2l_optional(&self.#field_name) {
                         characteristics.extend(nested_characteristics.into_iter().map(|mut c| {
-                            c.name = format!("{}.{}", #qualified_name, c.name);
+                            // Correctly format the name for nested characteristics, ensuring they are prefixed correctly
+                            c.name = format!("{}.{}", stringify!(#struct_name), c.name);
                             c
                         }));
+                    } else {
+                        // Only add the characteristic for the field if it's not a nested structure implementing Flatten
+                        characteristics.push(Characteristic {
+                            name: format!("{}.{}", stringify!(#struct_name), stringify!(#field_name)),
+                            datatype: stringify!(#field_type).to_string(),
+                        });
                     }
                 }
             });
@@ -43,14 +44,13 @@ pub fn flatten_derive(input: TokenStream) -> TokenStream {
                         characteristics
                     }
 
-                    // Optional to_a2l implementation, returns None for types that do not implement ToA2l
                     fn to_a2l_optional(&self) -> Option<Vec<Characteristic>> {
                         Some(self.a2l_flatten())
                     }
                 }
             }
         },
-        _ => panic!("Only structs are supported"),
+        _ => panic!("Flatten macro only supports structs"),
     };
 
     gen.into()
